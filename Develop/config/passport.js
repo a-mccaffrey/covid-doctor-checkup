7fb7d18c0c39211a6172f3904cceb1e6cf71c20d
a-mccaffrey <aisling.s.mccaffrey@outlook.com>
2020-08-26 21:04:21 -0400

    Added models and worked on passport.js

--- Develop/config/passport.js
+++ Develop/config/passport.js
@@ -3,8 +3,9 @@ var LocalStrategy = require("passport-local").Strategy;
 
 var db = require("../models");
 
+// Patient Login
 // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
-passport.use(new LocalStrategy(
+passport.use('patient-signup', new LocalStrategy(
   // Our user will sign in using an email, rather than a "username"
   {
     usernameField: "email"
@@ -33,6 +34,72 @@ passport.use(new LocalStrategy(
     });
   }
 ));
+
+// Doctor Login
+// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+passport.use('doctor-signup', new LocalStrategy(
+  // Our user will sign in using an email, rather than a "username"
+  {
+    usernameField: "email"
+  },
+  function(email, password, done) {
+    // When a user tries to sign in this code runs
+    db.User.findOne({
+      where: {
+        email: email
+      }
+    }).then(function(dbUser) {
+      // If there's no user with the given email
+      if (!dbUser) {
+        return done(null, false, {
+          message: "Incorrect email."
+        });
+      }
+      // If there is a user with the given email, but the password the user gives us is incorrect
+      else if (!dbUser.validPassword(password)) {
+        return done(null, false, {
+          message: "Incorrect password."
+        });
+      }
+      // If none of the above, return the user
+      return done(null, dbUser);
+    });
+  }
+));
+
+
+// Admin Login
+// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+passport.use('admin-signup', new LocalStrategy(
+  // Our user will sign in using an email, rather than a "username"
+  {
+    usernameField: "email"
+  },
+  function(email, password, done) {
+    // When a user tries to sign in this code runs
+    db.User.findOne({
+      where: {
+        email: email
+      }
+    }).then(function(dbUser) {
+      // If there's no user with the given email
+      if (!dbUser) {
+        return done(null, false, {
+          message: "Incorrect email."
+        });
+      }
+      // If there is a user with the given email, but the password the user gives us is incorrect
+      else if (!dbUser.validPassword(password)) {
+        return done(null, false, {
+          message: "Incorrect password."
+        });
+      }
+      // If none of the above, return the user
+      return done(null, dbUser);
+    });
+  }
+));
+
 
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user
